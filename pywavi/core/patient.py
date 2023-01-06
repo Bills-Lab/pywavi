b2d29661a8dd59c4bd21605eb4733dc08748c694
Race Peterson <racepeterson@MF00005.local>
2023-01-06 10:21:46 -0700

    more documentation

--- pywavi/core/patient.py
+++ pywavi/core/patient.py
@@ -7,6 +7,24 @@ import os
 import glob
 
 class Chunk:
+    """
+    A class used to represent an Animal
+
+    ...
+
+    Attributes
+    ----------
+    chunk_data : pandas dataframe
+        a pandas dataframe containing the eeg data from an event occuring within a test
+    chunk_name : str
+        the name of the event specifically coded for the test that is happening please see util for category translation
+
+    Methods
+    -------
+    average_chunk()
+        Averages the all individual node waves into one general wave returns numpy array
+    """
+
     def __init__(self, data) -> None:
         self.chunk_data = data # pd.df
         self.chunk_name = str(data["Event"][0])
@@ -16,25 +34,60 @@ class Chunk:
         return self.chunk_data[["FP1", "FP2", "F3", "F4", "F7", "F8", "C3", "C4", "P3", "P4", "O1", "O2", "T3", "T4", "T5", "T6", "FZ", "CZ", "PZ"]].mean(axis=1)
 
 class Patient:
+    """
+    A class used to represent an Animal
+
+    ...
+
+    Attributes
+    ----------
+    csv_path : str
+        a path pointing to the parsed WAVI dataframe
+    condition : str
+        the name of the condition that the patient has (OPTIONAL)
+    patient_identifier : str
+        identifier of the patient
+    eeg_df : pandas.DataFrame
+        dataframe from the csv_path entire dataset
+
+    Methods
+    -------
+    fast_fourier(node_name)
+        node_name : str
+            performs a fast fourier transorm on the node_name
+    
+    visualize_region(node_name)
+        node_name : str
+            creates a viz for a specific region of the brain
+    """
+
     def __init__(self, path, identifier=None, condition=None):
         self.csv_path = path
         self.condition = condition
         self.patient_identifier = identifier
-        self.name = os.path.basename(self.csv_path)[:4]
+        self.name = os.path.basename(self.csv_path)[:4] # this may be redundant 
         self.eeg_df = pd.read_csv(path)[["Event","FP1", "FP2", "F3", "F4", "F7", "F8", "C3", "C4", "P3", "P4", "O1", "O2", "T3", "T4", "T5", "T6", "FZ", "CZ", "PZ"]]
 
-    def linear_predictor_coefficient(self):
-        return
-    
-    def fast_fourier(self, seq):
-        '''param: seq pandas.Series of a specific region of the brain'''
-        name = seq.name
-        data = np.array(seq)
+    def fast_fourier(self, node_name:str):
+        '''param: node_name:str is the initialism of the region of the brain that you would like to '''
+        data = np.array(self.eeg_df[node_name])
         y = fft(np.array(data))
         xf = fftfreq(data.shape[0], 0.01)
         plt.plot(xf[:data.shape[0]], y[:data.shape[0]])
         plt.show()
         return
-
-    def wavelet(self):
+    
+    def visualize_region(self, node_name):
+        if node_name == "all":
+            data = self.eeg_df.iloc[:, 1:].mean(axis=1)
+        else:
+            data = self.eeg_df[node_name]
+        
+        fig, ax = plt.subplot()
+        X = list(range(len(data)))
+        Y = data.to_numpy().reshape(1,-1)
+        ax.plot(X, Y, '-gD', markevery=[self.three], label='Patient Impact')
+        ax.set_title("Wave of {}".format(node_name))
+        plt.legend()
+        plt.show()
         return
--- pywavi/core/pthree.py
+++ pywavi/core/pthree.py
@@ -1,13 +1,15 @@
 '''
-Race Peterson
+Race Peterson Noorda COM
 
-These classes digest a patient WAVI eeg file.
+These classes help organize data coming in from a P300 exam.
 
-Combining nodes and artifacts take some more nuanced details, so a professor is needed
+A P300 exam measure reaction time. The patient puts on headphones and listens to a series of beeps
+Every beep sounds the same but there is an "oddball" beep. When the oddball beep sounds the patient is instructed to click as fast as they can
 
+The data of the event is signified by the following
 0 - nothing
-1 - normal beat
-2 - oddball
+1 - normal beep
+2 - oddball beep
 3 - user input
 '''
 import pandas as pd
@@ -21,6 +23,25 @@ import os
 import math
 
 class PChunk(Chunk):
+    """
+    A class to collect all the events during a p300 test
+
+    ...
+
+    Attributes
+    ----------
+    three : int
+        the index in the data of where the user impact was
+
+    Methods
+    -------
+    find_impact()
+        searches through the dataset to find where the human impact is
+    combine_nodes()
+        averges all nodes together into one wave
+    vis_event_diff()
+        simple visualization of the chunk that shows the marker of where they made impact
+    """
     def __init__(self, data) -> None:
         super().__init__(data)
         self.three = self.find_impact()
@@ -31,17 +52,6 @@ class PChunk(Chunk):
                     return i
 
     def combine_nodes(self, method='avg'):
-        '''
-        This function takes a chunk and combines it.
-        This method will have multiple options
-        param: method
-            -'avg'
-                takes the average across all of the nodes at any given time
-            -'ffs'
-                Fourier fuse
-            - 'linear predicter analysis LPC'
-
-        '''
         if method == 'avg':
             return np.array(self.data_df.drop(["Event"], axis=1).mean(axis=1))
     
@@ -60,16 +70,44 @@ class PChunk(Chunk):
                 plt.show()
 
 class PThreeHundred(Patient):
-    def __init__(self, path, identifier=None, condition=None) -> None:
+    """
+    A class that represents the p300 test. Abstraacted from the Patient class
+    
+    ...
+
+    Attributes
+    ----------
+    event_index : a list of indexes that represent the location of events
+    all_chunks : a collection of all the PChunk objects for a specific test
+
+    Methods
+    -------
+    get_index()
+        returns a list of all the indexes where a event occurs
+    smoothing()
+        NOT IMPLEMENTED
+    chunk_csv()
+        creates a collection of chunks
+    combine_events()
+        takes all oddball events in the p300 test and averages them together
+    get_node_avg_of_chunk(node_name)
+        gets all chunks of the same event name and averages them together
+    get_node_and_integrate(node_name)
+        takes simpson integral of a node wave
+    combine_markers()
+        averages the impact time of the patient 
+
+    
+    """
+    def __init__(self, path, identifier=None, condition=None):
         super().__init__(path, identifier, condition)
-        self.csv_path = path
+        self.csv_path = path # not neccessary
         self.name = os.path.basename(self.csv_path)[:4]
-        self.eeg_df = pd.read_csv(path)[["Event","FP1", "FP2", "F3", "F4", "F7", "F8", "C3", "C4", "P3", "P4", "O1", "O2", "T3", "T4", "T5", "T6", "FZ", "CZ", "PZ"]]
-        self.get_index()
+        self.event_index = self.get_index()
         self.all_chunks = self.chunk_csv()
 
     def get_index(self):
-        self.event_index = [index for index, row in self.eeg_df.iterrows() if row["Event"] != 0]
+        return [index for index, row in self.eeg_df.iterrows() if row["Event"] != 0]
 
     def smoothing(self):
         '''There may need to be some smothing done to the data set in the event of a node disconnecting'''
@@ -78,19 +116,19 @@ class PThreeHundred(Patient):
     def chunk_csv(self):
         all_chunks = []
         for i in range(len(self.event_index) - 1):
-            all_chunks.append(Chunk(self.eeg_df.iloc[self.event_index[i]:self.event_index[i] + 500])) # Originally 100
+            all_chunks.append(PChunk(self.eeg_df.iloc[self.event_index[i]:self.event_index[i] + 500])) # Originally 100
         return all_chunks
     
     def combine_events(self):
         two_events = [chunk.combine_nodes() for chunk in self.all_chunks if chunk.name == 2]
         return np.sum(two_events, axis=0) / len(two_events)
     
-    def get_node_avg_of_chunk(self, heading:str):
-        all_node = [chunk.data_df[heading].to_numpy() for chunk in self.all_chunks]
+    def get_node_avg_of_chunk(self, node_name:str):
+        all_node = [chunk.data_df[node_name].to_numpy() for chunk in self.all_chunks]
         return np.sum(all_node, axis=0) / len(all_node)
 
-    def get_node_and_integrate(self, heading:str):
-        all_node = [abs(simpson(chunk.data_df[heading].to_numpy())) for chunk in self.all_chunks]
+    def get_node_and_integrate(self, node_name:str):
+        all_node = [abs(simpson(chunk.data_df[node_name].to_numpy())) for chunk in self.all_chunks]
         return sum(all_node) / len(all_node)
         # return all_node
 
